54a2d7a445c480f58fa897389c7c2cfe4f6ee113
Alexandr Zhuk <sanek201dg@ukr.net>
2019-01-22 20:11:00 +0200

    update

--- Node_JS/Classworking_three/index.js
+++ Node_JS/Classworking_three/index.js
@@ -1,65 +1,104 @@
-const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+// grab the user model
 const PORT = process.env.PORT || 3002;
 
 mongoose.Promise = global.Promise;
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
-
-const Schema = mongoose.Schema;
 mongoose.connect('mongodb://mirowa:Alexandr55@ds157064.mlab.com:57064/myfirstdb')
-const userSchema = new Schema({
-  name: String,
-  username: String,
-  password: String,
-  admin: Boolean,
-  location: String,
-  meta: {
-    age: Number,
-    wesite: String
-  },
-  created_at: Date,
-  updated_at: Date
-})
-
-const User = mongoose.model('User', userSchema);
-
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req,res) => res.send('hellow this is my site'));
-app.get('/user/', (req,res) => 
-  User.find({}, (err,users) => {
-    if(err) throw err;
-    res.send(users);
-  })
-);
+// разрешаем нашему серверу общаться с внешним миром
+app.use(cors());
+app.options('*', cors());
 
-app.get('/user_name/:name', (req, res) =>
-  User.find({name: req.params.name}, (err, users) =>{
-    if(err) throw err;
-    res.send(users)
-  })
-)
+app.get('/', (req, res) => res.send('hellow this is my site'));
+app.get('/users/', (req, res) => {
+    User.find({}, (err, users) => {
+        if (err) throw err;
+        res.json(users);
+    })
+});
 
-
-app.post('/user/', (req,res) =>{
-  let newUser = User({
-    name: req.body.name,
-    username: req.body.username,
-    password: req.body.password,
-    admin: true
-  });
-  newUser.save((err) =>{
-    if(err) throw err;
-    res.send(newUser)
-  })
+app.post('/user/', (req, res) => {
+    let newUser = User({
+        name: req.body.name,
+        username: req.body.username,
+        password: req.body.password,
+        admin: true,
+    })
+    newUser.save(function(err) {
+        if (err) throw err;
+        // console.log(`User created! ${newUser}`);
+        res.json(200, `User created! ${newUser}`);
+    })
 })
 
+app.get('/user_username/:username', (req, res) => {
+    User.find({username: req.params.username}, (err, data) => {
+        if (err) throw err;
+        res.json(data);
+    })
+});
 
+app.get('/user_id/:id', function (req,res){
+    //let reg=/req.params.name/
+    User.find({_id:req.params.id},(err,users)=>{
+        if (err) throw err;
+        res.send(users)
+    })
+});
+
+app.delete('/users_id/:id', (req, res) => {
+    User.findByIdAndRemove(req.params.id, (err) => {
+        if (err) throw err;
+        //we have deleted the user
+        // console.log('User deleted!');
+        res.json(200, 'User deleted!');
+    })
+})
+
+app.put('/users_put/:_id', (req, res) => {
+    User.findByIdAndUpdate(req.params._id, { username: req.body.username, password: req.body.password}, (err, user) => {
+        if (err) throw err;
+        //we have update the user
+        res.json(200, 'User update!');
+    })
+})
 
 app.listen(PORT, () => console.log(`Сервер запущен на порту ${PORT}`));
+
+
+
+
+
+
+
+
+// create new user 
+// let newUser = User({
+//     // модернизируем так наш сервер чтобы по запросук создавать юзера  
+//     username: 'Vitalii',
+//     password: 'Vitalii',
+// });
+
+//save the user function
+// newUser.save(function(err) {
+//     if (err) throw err;
+//     console.log(`User created! ${newUser}`);
+// })
+
+// app.get('/user_id/:id', (req, res) => {
+//     User.findById(ObjectId('req.params.id'), (err, users) => {
+//         if (err) throw err;
+//         res.send(users);
+//     })
+// });
+
